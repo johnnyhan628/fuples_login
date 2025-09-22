297fc397112de47d1aa2b0dc0a0eece60577a2b3
johnnyhan628 <johnnyhan628@gmail.com>
2025-09-22 22:18:29 +0900

    Remote git repository

---
+++ src/main/java/com/fuples/FuplesApplication.java
@@ -0,0 +1,13 @@
+package com.fuples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fuples/FuplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fuples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
