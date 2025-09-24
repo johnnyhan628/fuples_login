26d4c8d7f464ae2fda60f95c65638a85acb20edc
johnnyhan628 <johnnyhan628@gmail.com>
2025-09-25 00:12:31 +0900

    Chore: Setting Swagger

---
+++ src/main/java/com/fuples/user/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.fuples.user.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RequestMapping("/api/user")
+@RequiredArgsConstructor
+@RestController
+public class UserController {
+    
+}
